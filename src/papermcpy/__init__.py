5dd6bb98b59e7af49ac59cf774a216eacbaf0c66
Nevalicjus <50320623+Nevalicjus@users.noreply.github.com>
2022-01-19 22:34:43 +0100

    rewrote import

--- src/papermcpy/__init__.py
+++ src/papermcpy/__init__.py
@@ -1,5 +1,16 @@
 from .utils import paper_request
 
-from .download import *
-from .projects import *
-from .versions import *
+#from .download import *
+from .download import download
+
+#from .projects import *
+from .projects import proj
+from .projects import projs
+
+#from .versions import *
+from .versions import version
+from .versions import build
+from .versions import family
+from .versions import family_builds
+
+#import papermcpy.a import 
