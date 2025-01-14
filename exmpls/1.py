48ace4385485ec84462d2482e835015c242bffeb
Maciej Bromirski <maciej.bromirski@gmail.com>
2025-01-14 09:53:09 +0100

    examples fix

--- exmpls/1.py
+++
@@ -1,9 +0,0 @@
-import papermcpy, asyncio
-
-async def main(name):
-     proj = await papermcpy.proj(name)
-     for ver in proj.versions:
-             v = await papermcpy.version(name, ver)
-             print(f"Builds for {proj.name}'s version {v.version}: {v.builds}")
-
-asyncio.run(main("paper"))
